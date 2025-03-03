3ab31622b289663f0ad6fdd7341ce4bdc5c7d62a
cloreceh <hammitt1@msu.edu>
2025-03-03 01:30:33 +0000

    Updates to navigation

--- .eleventy.js
+++ .eleventy.js
@@ -3,4 +3,8 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+  return {
+    templateFormats: ["html", "liquid"],
+    htmlTemplateEngine: "liquid"
+  };
 };
